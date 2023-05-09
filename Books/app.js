500ca27add409f64e57e4debe106980390a7ad6e
Jose <t.jose51@gmail.com>
2023-05-09 13:12:30 -0400

    added new index and add book page changed around app.js code

--- Books/app.js
+++ Books/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
 app.use(express.static('public'));
 
 app.listen (port, () => {
     console.log('App listening on port http://localhost:3000');
+});
+
+app.get('/books', (req, res) => {
+   
 });
